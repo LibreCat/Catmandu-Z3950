fcc8c1aeac1433c8f4a2484329004e42b3dafeba
Emmanuel Di Pretoro <edipretoro@gmail.com>
2019-01-01 21:00:00 +0100

    Correcting some indentation issues

--- lib/Catmandu/Importer/Z3950/Parser/UNIMARC.pm
+++ lib/Catmandu/Importer/Z3950/Parser/UNIMARC.pm
@@ -8,17 +8,17 @@ has 'id' => (is => 'ro' , default => sub { '001'} );
 
 sub parse {
     my ($self,$str) = @_;
-		my $sysid = undef;
+    my $sysid = undef;
 
     return undef unless defined $str;
 
     my $record = MARC::Parser::RAW->new(\$str)->next();
-
-		foreach my $field (@$record) {
-			if ($field->[0] eq '001') {
-				$sysid = $field->[4];
-			}
-		}
+    
+    foreach my $field (@$record) {
+	if ($field->[0] eq '001') {
+		$sysid = $field->[4];
+	}
+    }
 
     return { _id => $sysid , record => $record };
 }
